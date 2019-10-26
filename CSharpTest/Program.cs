7e339bba5b204dd98e854738e96f1d9958788790
AB <AndrewBraunsdorf@gmail.com>
2019-10-26 17:12:29 -0400

    Enter Hello World!

--- CSharpTest/Program.cs
+++ CSharpTest/Program.cs
@@ -6,7 +6,20 @@ namespace CSharpTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter \"Hello World!\"");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Please enter \"Hello World!\"");
+            } else if (input == "Hello World!")
+            {
+                Console.WriteLine("Congratulations!!! Good Bye Now");
+            }
+            else
+            {
+                Console.WriteLine("Please enter \"Hello World!\"");
+            }
         }
     }
 }
